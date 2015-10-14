196d6962a9b645c3449d0079a8fe23de037f36e6
becxer <becxer87@gmail.com>
2015-10-15 08:59:40 +0900

    db connect

--- blog_url_crawler.js
+++ blog_url_crawler.js
@@ -21,12 +21,21 @@ console.log('post query : ' + post_query);
 console.log('------------------------------');
 
 //google search sample
+
+function randomIntInc (low, high) {
+    return Math.floor(Math.random() * (high - low + 1) + low);
+}
+
 google_list = [];
 list_add = function(g_query){
 	obj = function (callback){
 			console.log(g_query);
 			google.search(g_query, j, function(url){
-				callback(null,url);
+				next_time = randomIntInc(4000,10000);
+				console.log(next_time)
+				setTimeout(function() {
+					callback(null,url);
+				},next_time);
 			});
 		};
 	google_list.push(obj);
@@ -43,6 +52,14 @@ for (var i in place_list) {
 console.log('--------------------------');
 console.log(google_list);
 
+db_insert = function(db, url) {
+	db.serialize(function(){
+		var stmt = db.prepare("INSERT INTO blog(blog_area, blog_url) VALUES(?,?)");
+		stmt.run('hey',url); 
+		stmt.finalize();
+	});
+}
+
 async.series(google_list, 
 	function(err, results){
 		url_list = []
@@ -50,15 +67,13 @@ async.series(google_list,
 			url_list = url_list.concat(arguments[1][i]);
 		}
 		console.log(url_list)
+		var db = new sqlite3.Database('dpdb');
+		for(var i in url_list){
+			db_insert(db,url_list[i]);			
+		}
+		db.close();
 	}
 );
 
 /*
-			var db = new sqlite3.Database('dpdb');
-			db.serialize(function(){
-				var stmt = db.prepare("INSERT INTO blog(blog_area, blog_url) VALUES(?,?)");
-				stmt.run(place,url); 
-				stmt.finalize();
-			});
-			db.close();
 */

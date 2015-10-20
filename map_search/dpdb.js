36c3926dadfd0bb91c5799c923741cecd3cf31cd
corner <corner@hp100.net>
2015-10-20 21:48:05 +0900

    complete my first job

--- map_search/dpdb.js
+++ map_search/dpdb.js
@@ -1,31 +1,49 @@
 var sqlite3 = require('sqlite3').verbose();
+var util = require('util')
 var db;
 
 exports.init=function() {
-	db = new sqlite3.Database('../dpdb');
+	db = new sqlite3.Database('../dpdb');// new sqlite3.Database(':memory:'); 
 }
 
 exports.close=function() {
 	db.close();
 }
 
-exports.insert_store=function(values) {
+exports.insert_store=function(values, callback) {
 	//console.log(values);
 	db.serialize(function() {
 		var tbl_name = "store"
-		var tbl_fields = "(store_area, store_title, store_type, store_daum_id, store_daum_x, store_daum_y)"
-		try {
-			var stmt = db.prepare("INSERT INTO " + tbl_name + tbl_fields + " VALUES(?,?,?,?,?,?)");
-			stmt.run(values); 
-			stmt.finalize();
-		} catch(err) {
-			console.log("error in inserting: " + err.message);
-		}
+		var fields = "(store_daum_id, store_area, store_title, store_type, store_daum_x, store_daum_y)"
+		var stmt = db.prepare("INSERT INTO " + tbl_name + fields + " VALUES(?,?,?,?,?,?)");
+		stmt.run(values, function(err, row) {
+			if(err) 
+				console.log("error in inserting: " + err.message);
+			}); 
+		stmt.finalize();
 		
-		console.log("in DB");
-		db.each("SELECT * FROM " + tbl_name, function(err, row) {
-			console.log(row);
+		var id = "store_id"
+		db.get(util.format("SELECT %s FROM %s WHERE store_daum_id = %s", id, tbl_name ,values[0]), function(err, row) {
+			if(err) {
+				console.log("error in select: " + err.message);
+			} else if( row != undefined ) {
+				callback(row.store_id);
+			}
 		});
+	});
+}
+
+exports.insert_store_blog=function(values) {
+	//console.log(values);
+	db.serialize(function() {
+		var tbl_name = "store_blog"
+		var fields = "(blog_id, store_id)"
+		var stmt = db.prepare("INSERT INTO " + tbl_name + fields + " VALUES(?,?)");
+		stmt.run(values, function(err, row) {
+			if(err) 
+				console.log("error in inserting: " + err.message);
+			}); 
+		stmt.finalize();
 	});
 }
 
--- map_search/test.js
+++ map_search/test.js
@@ -5,19 +5,23 @@ var db = require('./dpdb.js');
 
 console.log(process.argv.length);
 
-if(process.argv.length < 4) {
-	console.log("usage: " + process.argv[1] + " [location] [store_name]");
+if(process.argv.length < 5) {
+	console.log("usage: " + process.argv[1] + " [location] [store_name] [blog_id]");
 	process.exit();
 }
 
 var loc_name = process.argv[2];
 var store_name = process.argv[3];
 var search_query = loc_name + " " + store_name;
+var blog_id = process.argv[4];
 
 map.get_map_info(search_query, function(loc_info) {
 		db.init();
-		var values = [loc_info["address"], loc_info["name"], loc_info["last_cate_name"], 
-			loc_info["confirmid"], loc_info["x"], loc_info["y"]];
-		db.insert_store(values); 
-		db.close();
+		var values = [ loc_info["confirmid"], loc_info["address"], loc_info["name"], loc_info["last_cate_name"], loc_info["x"], loc_info["y"]];
+		db.insert_store(values, function(store_id){
+			console.log(store_id);
+			values = [blog_id, store_id];
+			db.insert_store_blog(values);
+			db.close();
+		}); 
 });
